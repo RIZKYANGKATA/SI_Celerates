941e3506f89c88fe5cfea15e89d90ea4744e7239
RIZKYANGKATA <rizmadoris@gmail.com>
2024-03-27 10:30:28 +0700

    bootstrap & javascript

---
+++ praktik_6/script.js
@@ -0,0 +1,26 @@
+let message
+message = "Hello"
+
+alert(message)
+
+let message2 = "Hello, world!"
+alert(message2)
+
+let user = "Smith"
+let age = "21"
+let message3 = "Hello, Smith"
+alert(message3)
+
+let user2 = "John Doe",
+    age2= "21"
+    message2 = "Hello, John"
+alert(message4)
+
+
+//old script
+
+var message5 = "Hello, world!";
+
+const my_birthday = "13 Februari 2004";
+
+
